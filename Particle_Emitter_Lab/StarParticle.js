da7b077b9354faa2592e92704f1ac5e41e7d6e79
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-05-13 22:49:39 -0400

    Added star code
    Referenced assignment, Lab 6

--- Particle_Emitter_Lab/StarParticle.js
+++ Particle_Emitter_Lab/StarParticle.js
@@ -7,4 +7,31 @@ class StarParticle extends Particle {
     this.sides = int(random(5,8))
     this.step = random(1,2)
   }
+  
+  draw() {
+    push()
+    translate(this.position.x, this.position.y)
+    rotate(this.angle)
+    fill(this.color, this.lifespan)
+    noStroke()
+    beginShape()
+    let numPoints = this.sides * 2
+    let theta = 0
+    let dt = TWO_PI / numPoints
+    
+    for (let i = 0; i < numPoints; i++) {
+      let radius
+      if (i % 2 === 0) {
+        radius = this.innerRadius
+      } else {
+        radius = this.outerRadius
+      }
+      
+      let x = radius * cos(theta + dt * i)
+      let y = radius * sin(theta + dt * i)
+      vertex(x,y)
+    }
+    endShape()
+    pop()
+  }
 }
