8d230bf5aeb09477b2bb40aa0f3a06374b15c585
Matthew Lee <Matthew_Lee@avenues-ny.org>
2025-05-13 22:59:28 -0400

    Added necessary code for trails in file

--- Particle_Emitter_Lab/TrailParticle.js
+++ Particle_Emitter_Lab/TrailParticle.js
@@ -4,4 +4,21 @@ class TrailParticle extends Particle {
     this.trail = []
     this.maxTrail = 15
   }
+  
+  update() {
+    super.update()
+    this.trail.push(this.position.copy())
+  }
+  
+  draw() {
+    noFill()
+    stroke(this.color, this.lifespan)
+    beginShape()
+    
+    for (let i = 0; i < this.trail.length; i++) {
+      vertex(this.position.x, this.position.y)
+    }
+    endShape()
+    super.draw()
+  }
 }
